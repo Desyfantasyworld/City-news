79ff4e8322dae309009d4ffdafb1ed78d33fe65b
Desyfantasyworld <radhika6harma@gmail.com>
2025-06-08 15:08:17 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+
+fetch('articles.json')
+  .then(response => response.json())
+  .then(data => {
+    const container = document.getElementById('news-container');
+    data.forEach(article => {
+      const card = document.createElement('div');
+      card.className = 'article-card';
+      card.innerHTML = `
+        <img src="\${article.image}" alt="\${article.title}">
+        <div class="article-info">
+          <h3>\${article.title}</h3>
+          <p class="excerpt">\${article.excerpt}</p>
+          <div class="meta">
+            <span>\${article.date}</span> | <span>\${article.category}</span>
+          </div>
+        </div>
+      `;
+      container.appendChild(card);
+    });
+  })
+  .catch(error => console.error("Error loading articles:", error));
